af3529a37d70edb9f6b9ecc48a7c5c230c57f272
Raphaël <raphfar@gmail.com>
2022-07-13 15:00:01 +0200

    Created servlets and added images

--- ecommerce/src/main/java/controller/Header.java
+++ ecommerce/src/main/java/controller/Header.java
@@ -1,4 +1,4 @@
-package servlet;
+package controller;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class Accueil
+ * Servlet implementation class Header
  */
-@WebServlet("/accueil")
-public class Accueil extends HttpServlet {
+@WebServlet("/header")
+public class Header extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Accueil() {
+    public Header() {
         super();
         // TODO Auto-generated constructor stub
     }
