3545e25b4facbac2ba12c79821c4f4f9d8c6db6c
Mauduit-O <oum-el-khaire.mauduit@hetic.net>
2022-07-23 23:20:55 +0200

    fix js, added icon and css

--- ecommerce/src/main/webapp/js/modal-connexion.js
+++ ecommerce/src/main/webapp/js/modal-connexion.js
@@ -1,31 +1,45 @@
-let modalConnexion = document.querySelector(".background-modal");
-let closeConnexion = document.querySelector(".icon-close");
-let containerInputConnexion = document.querySelector(".container-input-modal-connexion");
-let connexion = document.querySelector(".connexion-click");
+let modalConnexion = document.querySelector(".background-modal-connexion");
+let close = document.querySelector(".icon-close");
+let containerInput1 = document.querySelector(".container-input-modal-connexion1");
+let containerInput2 = document.querySelector(".container-input-modal-connexion2");
+let IconConnexion = document.querySelector(".icon-connexion");
 
 
-connexion.addEventListener("click", () => {
+IconConnexion.addEventListener("click", () => {
 	modalConnexion.style.display = "flex";
 	
 	let input_email = document.createElement('input');
 	let input_password = document.createElement('input');
 	
+	let icon_email = document.createElement("img");
+	icon_email.id = 'icon-co';
+	let icon_pwd = document.createElement("img");
+	icon_email.id = 'icon-con';
+	
 	input_email.id = 'newEmail';
 	input_email.setAttribute('name','newEmail');
 	input_email.setAttribute('type','text');
 	input_email.setAttribute('placeholder','Email');
+	icon_email.src = "assets/icon/mail.svg";
 	
 	input_password.id = 'newPassword';
 	input_password.setAttribute('name','newPassword');
 	input_password.setAttribute('type','text');
 	input_password.setAttribute('placeholder','Mot de passe');
+	icon_pwd.src = "assets/icon/pwd.svg";
 	 
-	containerInputConnexion.appendChild(input_email);
-	containerInputConnexion.appendChild(input_password);
+	containerInput1.appendChild(icon_email); 
+	containerInput1.appendChild(input_email);
 	
-	closeConnexion.addEventListener("click", () => {
+	containerInput2.appendChild(icon_pwd);
+	containerInput2.appendChild(input_password);
+
+	
+	close.addEventListener("click", () => {
 		modalConnexion.style.display = "none";
 		input_email.remove();
 		input_password.remove();
+		icon_email.remove();
+		icon_pwd.remove();
 	});
 });
--- ecommerce/src/main/webapp/view/modals/modal-connexion.jsp
+++ ecommerce/src/main/webapp/view/modals/modal-connexion.jsp
@@ -1,22 +1,28 @@
 <div class="background-modal-connexion">
 	<div class="container-modal-connexion">
 		<div class="container-icon-close-connexion">
-			<img class="icon-close-connexion" alt="Icon fermer" src="assets/icon/close.svg">
+			<img class="icon-close" alt="Icon fermer" src="assets/icon/close.svg">
 		</div>
 		<div class="content-modal-connexion">
-			<h1 class="title-modal-connexion">Connexion</h1>
 			<form action="">
-				<div class="container-input-modal-connexion">
-				</div>
-				<div class="container-btn-modal-connexion">
-					<a><button class="btn-modal-connexion">Se connecter</button></a>
+				<div>
+					<h1 class="title-modal-connexion">Connexion</h1>
+					<div class="container-input-modal-connexion container-input-modal-connexion1">
+					</div>
+					<div class=" container-input-modal-connexion container-input-modal-connexion2">
+					</div>
+					<div class="container-btn-modal-connexion">
+						<a><button class="btn-modal-connexion">Se connecter</button></a>
+					</div>
 				</div>
 				<hr>
+			</form>
+			<div>
 				<h1 class="title-modal-inscription">S'inscrire</h1>
 				<div class="container-btn-modal-inscription">
 					<a><button class="btn-modal-inscription">S'inscrire</button></a>
 				</div>
-			</form>
+			</div>
 		</div>
 	</div>
 </div>
