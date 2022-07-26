392a07a7d5cd54054e71dbd81922045cc18c445d
Raphaël <raphfar@gmail.com>
2022-07-26 14:00:16 +0200

    detail produit js

--- ecommerce/src/main/webapp/js/details-produit.js
+++ ecommerce/src/main/webapp/js/details-produit.js
@@ -1,12 +1,12 @@
 let CommentsBtn = document.querySelector(".comments");
 let CommentsPanel = document.querySelector(".container-comments");
 
-
-
-
 CommentsBtn.addEventListener("click", () => {
 	
-	CommentsPanel.style.display = "flex";
+if	(CommentsPanel.style.display !== "none"){
+	CommentsPanel.style.display = "none";
+} else {
+	CommentsPanel.style.display = "";
+}
 	
 	});
-
