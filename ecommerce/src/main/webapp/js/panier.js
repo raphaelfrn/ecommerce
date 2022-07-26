b8d1df572257c8b0f121a40e4858ee6f5289c69f
Mauduit-O <oum-el-khaire.mauduit@hetic.net>
2022-07-26 12:21:59 +0200

    added js in basket page

---
+++ ecommerce/src/main/webapp/js/panier.js
@@ -0,0 +1,18 @@
+let basket = document.querySelector(".title-basket");
+let wishlist = document.querySelector(".title-wishlist");
+
+let containerBasket = document.querySelector(".container-basket");
+let containerWishlist = document.querySelector(".container-wishlist");
+
+console.log(basket);
+console.log(wishlist);
+
+wishlist.addEventListener('click', () => {
+	containerBasket.style.display="none";
+	containerWishlist.style.display="block";
+})
+
+basket.addEventListener('click', () => {
+	containerWishlist.style.display="none";
+	containerBasket.style.display="block";
+})
